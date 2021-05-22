7459c515afa1bcb2e852827167f21bbb9a22b3bb
RahelehHallaj <raheleh.hallaj@yahoo.com>
2021-05-22 15:23:50 +0430

    Init Project

---
+++ Application/Interfaces/Contexts/IDataBaseContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Contexts
+{
+    public interface IDataBaseContext
+    {
+
+        int SaveChanges();
+        int SaveChanges(bool acceptAllChangesOnSuccess);
+
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken());
+    }
+}
---
+++ Persistence/Contexts/DataBaseContext.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using Application.Interfaces.Contexts;
+using Domain.Attributes;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Contexts
+{
+    public class DataBaseContext : DbContext, IDataBaseContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+
+        //............................................................................
+        // افزودن خودکار فیلدهای Audit با استفاده از Shadow Property
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                if (entityType.ClrType.GetCustomAttributes(typeof(AuditableAttribute), true).Length > 0)
+                {
+                    builder.Entity(entityType.Name).Property<DateTime>("InsertTime");
+                    builder.Entity(entityType.Name).Property<DateTime?>("UpdateTime");
+                    builder.Entity(entityType.Name).Property<DateTime?>("RemoveTime");
+                    builder.Entity(entityType.Name).Property<bool>("IsRemoved");
+                }
+            }
+
+            base.OnModelCreating(builder);
+        }
+
+        public override int SaveChanges()
+        {
+            var modifiedEntries = ChangeTracker.Entries()
+                .Where(p => p.State == EntityState.Modified ||
+                p.State == EntityState.Added ||
+                p.State == EntityState.Deleted
+                );
+            foreach (var item in modifiedEntries)
+            {
+                var entityType = item.Context.Model.FindEntityType(item.Entity.GetType());
+
+                var inserted = entityType.FindProperty("InsertTime");
+                var updated = entityType.FindProperty("UpdateTime");
+                var RemoveTime = entityType.FindProperty("RemoveTime");
+                var IsRemoved = entityType.FindProperty("IsRemoved");
+
+                if (item.State == EntityState.Added && inserted != null)
+                {
+                    item.Property("InsertTime").CurrentValue = DateTime.Now;
+                }
+                if (item.State == EntityState.Modified && updated != null)
+                {
+                    item.Property("UpdateTime").CurrentValue = DateTime.Now;
+                }
+
+                if (item.State == EntityState.Deleted && RemoveTime != null && IsRemoved != null)
+                {
+                    item.Property("RemoveTime").CurrentValue = DateTime.Now;
+                    item.Property("IsRemoved").CurrentValue = true;
+                }
+            }
+            return base.SaveChanges();
+        }
+
+        //........................................................................
+
+    }
+}
+
