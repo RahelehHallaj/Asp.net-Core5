3405d27b71b2cd7dbe305bec16463897a7033fcb
RahelehHallaj <raheleh.hallaj@yahoo.com>
2021-05-23 19:07:29 +0430

    Add Register Section

--- WebSite.EndPoint/Controllers/AccountController.cs
+++ WebSite.EndPoint/Controllers/AccountController.cs
@@ -2,15 +2,66 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain.Users;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WebSite.EndPoint.Models.ViewModels.Register;
 
 namespace WebSite.EndPoint.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<User> _userManager;
+        public AccountController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
         public IActionResult Login()
         {
             return View();
         }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            User newuser = new User()
+            {
+                Email = model.Email,
+                UserName = model.Email,
+                FullName = model.FullName,
+                PhoneNumber = model.PhoneNumber
+            };
+            var result = _userManager.CreateAsync(newuser, model.Password).Result;
+            if (result.Succeeded)
+            {
+                return RedirectToAction(nameof(Profile));
+            }
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError(item.Code, item.Description);
+            }
+
+            return View(model);
+        }
+
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        
+
     }
 }
